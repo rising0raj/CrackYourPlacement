88dd71dd38dbef028b56f15e12950969f0fe10cb
https_rising <107956523+rising0raj@users.noreply.github.com>
2024-07-16 22:05:17 +0530

    Leetcode 75

---
+++ Sort Colors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low=0,mid=0,high=nums.length -1;
+        int temp;
+        while(high>=mid){
+            switch(nums[mid]){
+                case 0:
+                    temp = nums[low];
+                    nums[low] = nums[mid];
+                    nums[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    temp = nums[mid];
+                    nums[mid] = nums[high];
+                    nums[high] = temp;
+                    high--;
+                    break;
+            }                
+        }
+    }
+}
