14b1988cfe360d2031c8c4093e80d6a90650447f
https_rising <107956523+rising0raj@users.noreply.github.com>
2024-07-21 23:54:10 +0530

    leetcode 65

---
+++ Valid Number.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isNumber(String s) {
+        boolean numberSeen = false;
+        boolean eSeen = false;
+        boolean dotCheck = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                numberSeen = true;
+            } else if (ch == '+' || ch == '-') {
+                if (i > 0 && (s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E')){
+                    return false;
+                }
+            } else if (ch == '.') {
+                if (dotCheck || eSeen) {
+                    return false;
+                }
+                dotCheck = true;
+            } else if (ch == 'e' || ch == 'E') {
+                if (eSeen || !numberSeen) {
+                    return false;
+                }
+                eSeen = true;
+                numberSeen = false;
+            } else {
+                return false; 
+            }
+        }
+
+        return numberSeen;
+    }
+}
