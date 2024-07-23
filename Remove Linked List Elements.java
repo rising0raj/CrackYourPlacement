c6807fe454c7a73969485d7ee379613856aaeb27
https_rising <107956523+rising0raj@users.noreply.github.com>
2024-07-23 20:01:27 +0530

    Leetcode 203

---
+++ Remove Linked List Elements.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode temp = new ListNode(0);
+
+        temp.next = head;
+        ListNode current = temp;
+        while(current.next != null){
+            if(current.next.val == val){
+                current.next = current.next.next;
+
+        }else{
+           current = current.next; 
+        }
+        }
+
+        return temp.next;
+    }
+}
