ef0a63f989bf6af1b5fa6d4832a6fedb53653110
https_rising <107956523+rising0raj@users.noreply.github.com>
2024-07-25 16:01:29 +0530

    leetcode 445

---
+++ Add Two Numbers II.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        int carry = 0;
+        ListNode dummyHead = null;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
+            int val1 = stack1.isEmpty() ? 0 : stack1.pop();
+            int val2 = stack2.isEmpty() ? 0 : stack2.pop();
+            int total = val1 + val2 + carry;
+            carry = total / 10;
+            ListNode new_node = new ListNode(total % 10);
+            new_node.next = dummyHead;
+            dummyHead = new_node;
+        }
+
+        return dummyHead;        
+    }
+}
