32299e21fd427cb05d3ec8e9e2aa9452f09c0140
https_rising <107956523+rising0raj@users.noreply.github.com>
2024-07-29 21:05:57 +0530

    leetcode 225

---
+++ Implement Stack using Queues.java
@@ -0,0 +1,27 @@
+class MyStack {
+    Queue < Integer > q = new LinkedList < > ();
+
+    public MyStack() {
+        
+    }
+    
+    public void push(int x) {
+        q.add(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.add(q.remove());
+        }
+    }
+    
+    public int pop() {
+        return q.remove();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
