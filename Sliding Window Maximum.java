15389c8d041e58b4e29fe7ca7b795b79776d4ac5
https_rising <107956523+rising0raj@users.noreply.github.com>
2024-08-06 23:47:03 +0530

    Leetcode 239

---
+++ Sliding Window Maximum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n=nums.length, r[]=new int[n-k+1], ri=0;
+        Deque <Integer> q=new ArrayDeque<>();
+        
+        for(int i=0;i<n;i++){
+            if(!q.isEmpty() && q.peek()==i-k){
+                q.poll();
+            }
+            while(!q.isEmpty() && nums[q.peekLast()]<nums[i]){
+                q.pollLast();
+            }
+
+            q.offer(i);
+
+            if(i>=k-1){
+                r[ri++]=nums[q.peek()];
+            }
+        }
+
+        return r;
+
+    }
+}
