5ee3a778696213fe1e76fcd59d971a29baf167d8
https_rising <107956523+rising0raj@users.noreply.github.com>
2024-08-13 20:57:20 +0530

    Leetcode 300

---
+++ Longest Increasing Subsequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lengthOfLIS(int[] arr) {
+        List<Integer> temp = new ArrayList<>();
+        temp.add(arr[0]);
+
+        int len = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > temp.get(temp.size() - 1)) {
+
+                temp.add(arr[i]);
+                len++;
+            } else {
+                int ind = Collections.binarySearch(temp, arr[i]);
+
+                if (ind < 0) {
+                    ind = -ind - 1;
+                }
+
+                temp.set(ind, arr[i]);
+            }
+        }
+
+        return len;
+    }
+}
