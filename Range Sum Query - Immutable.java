a15f5a82357f6fbf8b96550b16465e53ef12c9d7
https_rising <107956523+rising0raj@users.noreply.github.com>
2024-08-15 01:20:58 +0530

    Leetcode 303

---
+++ Range Sum Query - Immutable.java
@@ -0,0 +1,13 @@
+class NumArray {
+    int arr[];
+    public NumArray(int[] nums) {
+        this.arr = nums;
+    }
+    
+    public int sumRange(int left, int right) {
+        int sum = 0;
+        for(int i = left;i<=right;i++)
+            sum += this.arr[i];
+        return sum;
+    }
+}
