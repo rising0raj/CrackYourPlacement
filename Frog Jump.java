3f325e212d6df62109d2ebda04f06219f309b8e3
https_rising <107956523+rising0raj@users.noreply.github.com>
2024-08-16 22:12:25 +0530

    Leetcode 403

---
+++ Frog Jump.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canCross(int[] stones) {
+        int n = stones.length;
+        boolean[][] dp = new boolean[n][n + 1];
+        dp[0][1] = true;
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int k = stones[i] - stones[j];
+                if (k <= j + 1) {
+                    dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
+                    if (i == n - 1 && dp[i][k]) 
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+}
